8fd27aa97e11acccd20aa4bd315363fd553076c4
Shota Kawashima <bysk.333@gmail.com>
2018-02-14 00:06:49 +0900

    funtousic追加

--- components/works.vue
+++ components/works.vue
@@ -11,28 +11,39 @@ section#works-list
         .workgrid
           p.h 活動
           p.main 課外活動
+          p.h 活動人数
+          p.main 2人
           p.h 担当
-          p.main デザイン（一部）・実装
+          p.main デザイン（一部） / 実装
           p.h 制作期間
           p.main 3 weeks
           p.h 使用技術
-          p.main Illustrator / HTML / SCSS
+          p.main Illustrator / HTML,SCSS
+
         p.summary 函館の魅力を伝える映像を制作するプロジェクト「ハコダテソラカメラ」の活動記録として制作しました。
-    .item
+    a.item(href="http://skawashima.github.io/skawashima.com/funtousic/" target="_blank")
       img.thumb(src="~/static/funtousic.png")
       .desc
         h1 funtousic
+          span.genre.genre-proposal 提案
           span.genre.genre-web Web
+          span.genre.genre-movie Movie
+          span.genre.genre-music Music
         .workgrid
           p.h 活動
-          p.main 課外活動
+          p.main 授業課題
+          p.h 活動人数
+          p.main 4人
           p.h 担当
-          p.main デザイン（一部）・実装
+          p.main 提案 / Webサイト制作 / イメージ映像
           p.h 制作期間
-          p.main 3 weeks
+          p.main 2 month
           p.h 使用技術
-          p.main Illustrator / HTML / SCSS
-        p.summary 函館の魅力を伝える映像を制作するプロジェクト「ハコダテソラカメラ」の活動記録として制作しました。
+          p.main HTML,CSS / AviUtl / Ableton Live
+        p.summary 触れられない物を触れられる「タンジブル」な提案をする授業課題の成果として制作しました。
+        p.summary <b>音を触ることが出来る球体デバイス</b>を提案し、それをプロモーションするWebサイトを制作しました。
+          | また、音を触るイメージを持ってもらうための映像と、その映像に使うための楽曲を制作しました。
+        p.summary この提案は、授業成果発表会「はこだて未来展2014冬」において最優秀賞に選ばれました。
 </template>
 
 <script>
@@ -110,17 +121,21 @@ a
         width: 470px
         text-align: left
         .genre
-          padding: 3px
+          padding: 4px 5px
           margin-top: 10px
           margin-left: 10px
           font-size: .7em
           border-radius: 5px
-          font-weight: normal
+          font-weight: bold
           color: #fff
         .genre-web
           background-color: #0C4842
         .genre-proposal
           background-color: #9F353A
+        .genre-movie
+          background-color: #08192D
+        .genre-music
+          background-color: #4A225D
         .workgrid
           margin-top: 15px
           display: grid
