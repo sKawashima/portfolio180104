d312349be85df9c28c7a5049f10ac16b370718ae
Shota Kawashima <bysk.333@gmail.com>
2018-02-14 01:26:00 +0900

    add hmp-pre and aokuototo

--- components/works.vue
+++ components/works.vue
@@ -3,6 +3,44 @@ section#works-list
   .backline
     .line
   .items
+    a.item(href="https://photos.app.goo.gl/PtQuQ569RDGNlrz92" target="_blank")
+      img.thumb(src="~/static/aokuototo.jpg")
+      .desc
+        h1 個人展覧会「青ク音ト」
+          span.genre.genre-movie Movie
+          span.genre.genre-music Music
+          span.genre.genre-art Art
+        .workgrid
+          p.h 活動
+          p.main アーティスト活動
+          p.h 活動人数
+          p.main 5人
+          p.h 担当
+          p.main 作曲 / 映像制作 / 空間演出 / 公開作曲 / 共同作曲
+          p.h 開催期間
+          p.main 2016/10/26 - 2016/11/25
+          p.h 使用技術
+          p.main Ableton Live / After Effects / Premire Pro
+        p.summary 駅前「はこだてみらい館」の一部を借りて楽曲とその楽曲のMVを展示。
+        p.summary はこだてみらい館発の函館のアーティストによる館内演出企画。
+        p.summary [補足]はこだてみらい館の館内BGMは全て川島の作品。
+    a.item(href="http://hakodate-miraiproject.jp/pre-comming/" target="_blank")
+      img.thumb(src="~/static/hmp-pre.png")
+      .desc
+        h1 はこだてみらいプロジェクト プレサイト
+          span.genre.genre-web Web
+        .workgrid
+          p.h 活動
+          p.main 課外活動
+          p.h 活動人数
+          p.main 1人
+          p.h 担当
+          p.main デザイン（一部） / 実装全般
+          p.h 制作期間
+          p.main 1day
+          p.h 使用技術
+          p.main Illustrator / HTML,Sass / PHP
+        p.summary 函館駅前に新たに設置される「はこだてみらい館」と「はこだてキッズプラザ」の告知のため、運営コンソーシアムより依頼され制作。
     a.item(href="http://soracamera.com/" target="_blank")
       img.thumb(src="~/static/soracamera.png")
       .desc
@@ -16,11 +54,27 @@ section#works-list
           p.h 担当
           p.main デザイン（一部） / 実装
           p.h 制作期間
-          p.main 3 weeks
+          p.main 3weeks
           p.h 使用技術
           p.main Illustrator / HTML,SCSS
-
-        p.summary 函館の魅力を伝える映像を制作するプロジェクト「ハコダテソラカメラ」の活動記録として制作しました。
+        p.summary 函館の魅力を伝える映像を制作するプロジェクト「ハコダテソラカメラ」の活動記録として制作した。
+    .item
+      iframe.thumb.thumb-youtube(src="https://www.youtube.com/embed/Ghyb2fYJDUo?rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen)
+      .desc
+        h1 はこだて未来展2015 リフレクションムービー
+          span.genre.genre-movie Movie
+        .workgrid
+          p.h 活動
+          p.main 課外活動
+          p.h 活動人数
+          p.main 2人
+          p.h 担当
+          p.main 映像編集
+          p.h 制作期間
+          p.main 1day(イベント当日)
+          p.h 使用技術
+          p.main AviUtl
+        p.summary イベントで起きたことをその日の終わりに見て振り返るために当日中に制作した。
     a.item(href="http://skawashima.github.io/skawashima.com/funtousic/" target="_blank")
       img.thumb(src="~/static/funtousic.png")
       .desc
@@ -35,15 +89,15 @@ section#works-list
           p.h 活動人数
           p.main 4人
           p.h 担当
-          p.main 提案 / Webサイト制作 / イメージ映像
+          p.main 提案 / Webサイト制作 / イメージ映像 / 映像BGM
           p.h 制作期間
-          p.main 2 month
+          p.main 2months
           p.h 使用技術
           p.main HTML,CSS / AviUtl / Ableton Live
-        p.summary 触れられない物を触れられる「タンジブル」な提案をする授業課題の成果として制作しました。
-        p.summary <b>音を触ることが出来る球体デバイス</b>を提案し、それをプロモーションするWebサイトを制作しました。
-          | また、音を触るイメージを持ってもらうための映像と、その映像に使うための楽曲を制作しました。
-        p.summary この提案は、授業成果発表会「はこだて未来展2015」において最優秀賞に選ばれました。
+        p.summary 触れられない物を触れられる「タンジブル」な提案をする授業課題の成果として制作した。
+        p.summary <b>音を触ることが出来る球体デバイス</b>を提案し、それをプロモーションするWebサイトを制作した。
+          | また、音を触るイメージを持ってもらうための映像と、その映像に使うための楽曲を制作した。
+        p.summary この提案は、授業成果発表会「はこだて未来展2015」において最優秀賞を受賞。
 </template>
 
 <script>
@@ -108,7 +162,7 @@ a
       height: auto
       width: 100%
       transition: .3s
-      margin-top: 20px
+      margin-top: 50px
       padding: 10px
       border: 1px rgba(#4e1a68, 0) dotted
       box-sizing: border-box
@@ -117,6 +171,8 @@ a
         height: auto
         align-self: flex-start
         box-shadow: 0 0 8px gray
+      .thumb-youtube
+        height: calc(470px / 560 * 315)
       .desc
         width: 470px
         text-align: left
@@ -136,6 +192,8 @@ a
           background-color: #08192D
         .genre-music
           background-color: #4A225D
+        .genre-art
+          background-color: #946243
         .workgrid
           margin-top: 15px
           display: grid
